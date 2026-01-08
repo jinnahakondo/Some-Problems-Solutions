f826ca36864ede19ec495f4e1fc0a2aaec1eb475
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-01-08 09:16:02 +0600

    pingPong problem solved

---
+++ pingPong.js
@@ -0,0 +1,17 @@
+const getPingPong = () => {
+    for (let i = 1; i <= 20; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('pingPong');
+        }
+        else if (i % 3 === 0 ) {
+            console.log('ping');
+        }
+        else if (i % 5 === 0) {
+            console.log('pong');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+getPingPong()
